99263f597099e678c540fd30144c42ea0d56d7b8
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-04-28 13:41:30 -0500

    more controllers

---
+++ OkGroomer/Controllers/NoteController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using OkGroomer.Models;
+using OkGroomer.Repositories;
+using System;
+using System.Collections.Generic;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OkGroomer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly INoteRepository _noteRepo;
+
+        public NoteController(INoteRepository noteRepo)
+        {
+            _noteRepo = noteRepo;
+        }
+
+
+
+        // GET: api/<OwnerController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_noteRepo.GetAll());
+        }
+
+        // GET api/<OwnerController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_noteRepo.GetNoteById(id));
+        }
+
+        // POST api/<OwnerController>
+        [HttpPost]
+        public IActionResult Post(Note note)
+        {
+            _noteRepo.Add(note);
+            return CreatedAtAction("Get", new { id = note.Id }, note);
+        }
+
+        // PUT api/<OwnerController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Note note)
+        {
+
+            if (id != note.Id)
+            {
+                Console.WriteLine("id: ", id, "post id: ", note.Id);
+                return BadRequest();
+            }
+
+            _noteRepo.Edit(id, note);
+            return Ok(note);
+        }
+
+        // DELETE api/<OwnerController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _noteRepo.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ OkGroomer/Repositories/NoteRepository.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Extensions.Configuration;
+using OkGroomer.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Linq;
+using Tabloid.Repositories;
+using Tabloid.Utils;
+
+namespace OkGroomer.Repositories
+{
+    public class NoteRepository : BaseRepository, INoteRepository
+    {
+        public NoteRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Note> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, DogId, GroomerId, Content FROM Note";
+
+                    var notes = new List<Note>();
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var note = new Note()
+                        {
+                            Id = DbUtils.GetInt(reader, "id"),
+                            DogId = DbUtils.GetInt(reader, "DogId"),
+                            GroomerId = DbUtils.GetInt(reader, "GroomerId"),
+                            Content = DbUtils.GetString(reader, "Content")
+                        };
+                        notes.Add(note);
+                    }
+                    reader.Close();
+
+                    return notes;
+                }
+            }
+        }
+
+        public Note GetNoteById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                    Select
+                                        Id, 
+                                        DogId,
+                                        GroomerId,
+                                        Content
+                                    From Note
+                                    WHERE Id = @id";
+                    DbUtils.AddParameter(cmd, "@id", id);
+
+                    var reader = cmd.ExecuteReader();
+                    Note note = null;
+                    if (reader.Read())
+                    {
+                        note = new Note()
+                        {
+                            Id = DbUtils.GetInt(reader, "id"),
+                            DogId = DbUtils.GetInt(reader, "DogId"),
+                            GroomerId = DbUtils.GetInt(reader, "GroomerId"),
+                            Content = DbUtils.GetString(reader, "Content")
+                        };
+                    }
+                    reader.Close();
+                    return note;
+                }
+            }
+        }
+
+        public void Add(Note note)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Note (DogId, GroomerId, Content)
+                                        OUTPUT INSERTED.ID
+                                        VALUES (@DogId, @GroomerId, @Content, )";
+                    DbUtils.AddParameter(cmd, "@DogId", note.DogId);
+                    DbUtils.AddParameter(cmd, "@GroomerId", note.GroomerId);
+                    DbUtils.AddParameter(cmd, "@Content", note.Content);
+
+                    note.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+        public void Edit(int id, Note note)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Note
+                                        SET 
+                                            DogId = @DogId,
+                                            GroomerId = @GroomerId,
+                                            Content = @Content
+                                        WHERE Id = @id";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    DbUtils.AddParameter(cmd, "@DogId", note.DogId);
+                    DbUtils.AddParameter(cmd, "@GroomerId", note.GroomerId);
+                    DbUtils.AddParameter(cmd, "@Content", note.Content);
+
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Note WHERE Id = @id";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
