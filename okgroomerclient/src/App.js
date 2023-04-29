fc25ef25822f69086e0211260ddb94c367746af8
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-04-29 17:45:32 -0500

    started frontend

--- okgroomerclient/src/App.js
+++ okgroomerclient/src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from './Header';
-import { useState } from 'react';
-import ApplicationViews from './ApplicationViews';
+import { useState,useEffect } from 'react';
+import ApplicationViews from './components/ApplicationViews';
+import { onLoginStatusChange,me,logout } from './Modules/authManager';
+import { Spinner } from "reactstrap";
+
 
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(null);
   const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    onLoginStatusChange(setIsLoggedIn);
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      me().then((userPro) => {
+        if (userPro.activeStatus === "Disabled") {
+          logout();
+        } else {
+          setUserProfile(userPro);
+        }
+      });
+    } else {
+      setUserProfile(null);
+    }
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    if (userProfile) {
+      if (userProfile.activeStatus === "Deactivated") {
+        setIsLoggedIn(false);
+      }
+    }
+  }, [userProfile]);
+
+  if (isLoggedIn === null) {
+    return <Spinner className="app-spinner dark" />;
+  }
   return (
     <Router>
       <Header isLoggedIn={isLoggedIn} userProfile={userProfile} />
