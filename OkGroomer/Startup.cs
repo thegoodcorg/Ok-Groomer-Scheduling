2c9928d651b01644946c6211ab9cc8a09f756a56
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-06-13 22:49:59 -0500

    deployment prep

--- OkGroomer/Startup.cs
+++ OkGroomer/Startup.cs
@@ -90,7 +90,7 @@ namespace OkGroomer
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OkGroomerV1"));
             }
 
-            app.UseCors(builder => builder.WithOrigins("<http://localhost:3000>").AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
 
 
             app.UseHttpsRedirection();
